f1170dd6c1c1dfcb73dff7551e3249e3e3b5fbad
fahimpjee1988 <adjmal.ra@gmail.com>
2020-07-09 14:39:45 +0200

    test.12.weird

---
+++ assets/js/main.js
@@ -0,0 +1,114 @@
+
+//     let hi = `Superwoman`;
+//     let HiS = `Hello my name is `;
+
+// function Sup () {
+
+// let x = $(HiS) +""+$(hi);
+
+    
+
+//     console.log(x)
+
+
+// }
+
+// function sup2(hero){
+//     console.log(`Ì am`+hero)
+// }
+
+// function sup3(){
+// console.log(`Superman`)
+// }
+
+// function now(){
+//     console.log($(sup2)+$(sup3))
+// }
+
+
+
+
+// function whereFrom(){
+//     let x= 'hello my Name is Adjmal'
+// let y= 'I am from düsseldorf'
+// let z= 'i am 30 years old '
+//     console.log($(x)+""+$(y)+""+$(z))
+
+
+// function func1(){
+//     console.log('I am Adjmal')
+// }
+// function func2(age){
+//     let age= 30
+//     console.log('I am'+age)
+// }
+
+// function func3(city){
+//     let city = "Düsseldorf"
+//     console.log ($(func1)+$(func2)+$(func3))
+// }
+
+// function greetings(age, city, name){
+//     console.log (`Hello I am ${age}, I am from ${city}, My Name is ${name} )
+// }
+
+
+let list = 'Home, About and Contact';
+let home = list.slice(0, 4);
+let about = list.slice(6, 11);
+let contact =list.slice(16, 23);
+
+console.log(home, about, contact)
+//Lev1_1_js-einführung_template-literals
+
+
+let Name = "adjmal "
+let Nachname ="rasoul"
+let Alter  = 31
+let Geburtsort = "kabul"
+let Groeße = "177cm"
+let Gewicht = "90kg+"
+let Hobbys = "kampfsport"
+let Computerspiele ="PS4 wenn überhaupt"
+let Lieblingsessen = "Pizza, Bürger"
+let Liebslingsport =" Boxen, Bjj"
+let Lieblingsbekleidungshersteller = "Hemden" 
+let Lieblingsjahreszeit = "sommer"
+let Lieblingsurlaubsort = "prague"
+
+
+
+console.log("Ich heisse "+Name+""+Nachname+" ich bin " +" Ich bin "+Alter+
+" Ausserdem komme ich aus "+Geburtsort+" Desweiteren bin ich " + Groeße +"und wiege" + Gewicht + " Meine hobby is " + Hobbys +"Ausserdem spiele ich "+Computerspiele+" Mein liebsling Essen sind"+Lieblingsessen+ "Was meine Liebslingsportarten sind, würde ich sagen "+Liebslingsport+ "Was ich auch sehr liebe is im "+Lieblingsjahreszeit+""+" zu tragen"+" Meine lieblingscity is "+Lieblingsurlaubsort)
+
+
+
+var lunatic = " The lunatic is on the grass";
+var thelunatic ="The lunatic is in my head "
+var darkSide = "I'll see you on the dark side of the moon";
+var hall = "hall";
+var head = "The lunatic is in my head";
+var door = "You lock the door";
+var paperboy = "paperboy";
+
+
+
+console.log(` ${lunatic}${lunatic}Remembering games and daisy chains and laughs 
+
+Got to keep the loonies on the path   
+The lunatic is in the hall The lunatics are in my hall 
+
+The paper holds their folded faces to the floor And every day the paperboy brings more
+
+And if the dam breaks open many years too soon And if there is no room upon the hill 
+
+And if your head explodes with dark forebodings too
+${darkSide}. ${head}${head}You raise the blade, 
+
+you make the change You rearrange me 'til I'm sane You lock the door And throw away the key There's someone in my head but it's not me
+
+And if the cloud bursts, thunder in your ear You shout and no one seems to hear And if the band you're in starts playing different tunes
+${darkSide}.
+
+
+`)
